45510e4f61b998f73f65af5065fa939840076907
Chalry Liu <287428729@qq.com>
2019-12-10 13:31:20 +0800

    Update sharp.rs

--- sharp.rs
+++ sharp.rs
@@ -1,29 +1,38 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type int=i32;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type long =i64;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type bit = i8;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type short =i16;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type string=String;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type ushort=u32;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[allow(non_camel_case_types)]
     type float=u64;
-    // type var= let;
-    // type function=fn;
-    // type List=Vec;
-    // type Tuple=tuple;
-    // type Interface=trait;
-    // type Struct= struct;
-    // type this=self;
-    // type public= pub;
-    // type Public= pub;
-    // type Package = mod;
-    // type Changeable = mut;
-    // type null=None;
 
 fn main() {
     println!("Hello, world!");
