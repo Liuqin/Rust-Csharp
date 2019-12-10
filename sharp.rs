a23bbd4089294ed2ec09c927cff01d6895443f73
Chalry Liu <287428729@qq.com>
2019-12-10 12:27:36 +0800

    Create sharp.rs

---
+++ sharp.rs
@@ -0,0 +1,23 @@
+mod sharp
+{
+    type import=use;
+    type int=i32;
+    type long =i64;
+    type bit = i8;
+    type short =i16;
+    type string=String;
+    type null=None;
+    type single=u32;
+    type float=u64;
+    type var= let;
+    type function=fn;
+    type List=Vec;
+    type Tuple=tuple;
+    type Interface=trait;
+    type Struct= struct;
+    type this=self;
+    type public= pub;
+    type Public= pub;
+    type Package = mod;
+    type Changeable = mut;
+}
