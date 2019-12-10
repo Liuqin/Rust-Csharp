ccd6ccabc2b2b4bec93bf06aa480dd35715f3a4a
Chalry Liu <287428729@qq.com>
2019-12-10 13:16:26 +0800

    Update sharp.rs

--- sharp.rs
+++ sharp.rs
@@ -1,23 +1,25 @@
-mod sharp
-{
-    type import=use;
+
     type int=i32;
     type long =i64;
     type bit = i8;
     type short =i16;
     type string=String;
-    type null=None;
-    type single=u32;
+    type ushort=u32;
     type float=u64;
-    type var= let;
-    type function=fn;
-    type List=Vec;
-    type Tuple=tuple;
-    type Interface=trait;
-    type Struct= struct;
-    type this=self;
-    type public= pub;
-    type Public= pub;
-    type Package = mod;
-    type Changeable = mut;
+    // type var= let;
+    // type function=fn;
+    // type List=Vec;
+    // type Tuple=tuple;
+    // type Interface=trait;
+    // type Struct= struct;
+    // type this=self;
+    // type public= pub;
+    // type Public= pub;
+    // type Package = mod;
+    // type Changeable = mut;
+    // type null=None;
+
+fn main() {
+    println!("Hello, world!");
 }
+
